93def159d46b5cbd10e4a6f5608e43936cbf0edb
njerimk <nmurugikamau@gmail.com>
2023-12-07 15:59:47 -0500

    added mongodb connection in config file, mongodb connection code in index.js, models folder with book schema and post request in index.js file to create a new instance of book '

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,48 @@
 import express from "express";
-import { PORT } from "./config.js";
+import mongoose from "mongoose";
+import { PORT, mongoDBURL } from "./config.js";
+import { Book } from './models/bookModel.js';
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`);
-})
+app.get("/", (request, response) =>{
+    console.log(request);
+    return response.status(234).send("Welcome to MERN Stack Tutorial")
 
+});
+
+//Route for Saving a new Book
+
+app.post('/books', async(request, response)=> {
+    try{
+        if(
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ){
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear'
+            });
+        }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+    } catch(error){
+        console.log(error.message);
+        response.status(500).send({ message: error.message});
+    }
+});
+
+//mongoosejs.com
+mongoose
+    .connect(mongoDBURL)
+    .then(()=> {
+        console.log('App connected to database');
+        console.log(`App is listening to port: ${PORT}`)
+
+    })
+    .catch((error)=> {
+        console.log(error);
+    })
