da4d2ff62cdc6e1add56321b6f1d599a3fc9dbd2
Njeri Kamau <64022619+njerimk@users.noreply.github.com>
2023-12-07 17:34:02 -0500

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
 export const mongoDBURL = 
-'mongodb+srv://jellieb:Moonfire!89@cluster0.zdsss4y.mongodb.net/books-collection?retryWrites=true&w=majority'
+'mongodb+srv://*****:*****@cluster0.zdsss4y.mongodb.net/books-collection?retryWrites=true&w=majority'
